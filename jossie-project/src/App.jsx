1a07e3283040bcd7d1fee9967ca04f2ba20f638c
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-08-14 11:03:07 -0700

    Set up project structure and simple nav bar as jsx

---
+++ jossie-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+
+function App() {
+      return (
+            <div className="App">
+                  <Navbar />
+                  <p>Jossie's Website </p>
+            </div>
+      );
+}
+
+export default App;
---
+++ jossie-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+      return (
+            // the "html" goes here so write "normally"
+            // w3schools 
+            <div class="topnav">
+                  <img src="src/assets/images/icon.png" width="42"></img>
+                  <a class="active" href="#home">Home</a>
+                  <a href="#news">News</a>
+                  <a href="#contact">Contact</a>
+                  <a href="#about">About</a>
+            </div >
+      );
+}
